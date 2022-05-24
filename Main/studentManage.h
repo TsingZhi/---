4c6bd27f09972fd7f1b30b277c1db637e8c0b3bc
TsingZhi <617565347@qq.com>
2022-05-24 13:04:14 +0800

    已重新格式化

--- Main/studentManage.h
+++ Main/studentManage.h
@@ -1,7 +1,7 @@
 #define SUGLEN sizeof(SUG)
 #define SPGLEN sizeof(SPG)
 
-enum Sex{female,male};  // 0为女，1为男
+enum Sex{female,male};			// 0为女，1为男
 struct underGraduate //本科生
 {
 	int id;						//学号
@@ -16,25 +16,25 @@ typedef struct underGraduate SUG; //本科生的类型定义
 
 struct postGraduate //研究生
 {
-	int id;				 //学号
-	char name[16];		 //姓名
-	enum Sex sex;		 //性别
-	char profession[30]; //专业
-	char class[30];		 //班级
-	char direction[30];	 //研究方向
-	char teacher[16];	 //导师
+	int id;					   //学号
+	char name[16];			   //姓名
+	enum Sex sex;			   //性别
+	char profession[30];	   //专业
+	char class[30];			   //班级
+	char direction[30];		   //研究方向
+	char teacher[16];		   //导师
 	int score[5];			   // 0-4分别为综合课程 论文 总成绩 班排名 校排名
 	struct postGraduate *next; //下一个节点
 };
-typedef struct postGraduate SPG;//研究生的类型定义
+typedef struct postGraduate SPG; //研究生的类型定义
 
 SUG *SUGHead = NULL, *SUGTail = NULL; // List:本科生链表头节点;Tail:尾节点
 SPG *SPGHead = NULL, *SPGTail = NULL; // List:研究生链表头节点;Tail:尾节点
-SUG SUGpage[30];//分页输出用，每十个节点记录一次
-SPG SPGPage[30];//分页输出用，每十个节点记录一次
+SUG SUGpage[30];					  //分页输出用，每十个节点记录一次
+SPG SPGPage[30];					  //分页输出用，每十个节点记录一次
 
 //申请新的本科生节点，尾指针自动置NULL，成绩初始化为-1
-SUG* newSUGNode()
+SUG *newSUGNode()
 {
 	SUG *new;
 	int i;
@@ -46,17 +46,17 @@ SUG* newSUGNode()
 	else
 	{
 		new->next = NULL;
-		for (i = 0; i < 6; i++)//初始化成绩
+		for (i = 0; i < 6; i++) //初始化成绩
 			new->score[i] = -1;
 		return new;
 	}
 }
 //申请新的研究生节点，尾指针自动置NULL，成绩初始化为-1
-SPG* newSPGNode()
+SPG *newSPGNode()
 {
 	SPG *new;
 	int i;
-	if((new = (SPG*)malloc(SPGLEN)) == NULL)
+	if ((new = (SPG *)malloc(SPGLEN)) == NULL)
 	{
 		printf("申请新的研究生节点失败.");
 		exit(0);
@@ -64,7 +64,7 @@ SPG* newSPGNode()
 	else
 	{
 		new->next = NULL;
-		for (i = 0; i < 5; i++)//初始化成绩
+		for (i = 0; i < 5; i++) //初始化成绩
 			new->score[i] = -1;
 		return new;
 	}
